7256e04063e176bec919c565b2528a18f844144f
DanieleFic <reb_ficus22@hotmail.it>
2022-03-07 17:39:00 +0100

    push per giovanni

--- database/seeds/Travels.php
+++ database/seeds/Travels.php
@@ -16,12 +16,12 @@ class Travels extends Seeder
     public function run(Faker $faker)
     {
         for ($i=0; $i < 20; $i++) { 
-            $newTravel = new Travel();
+            $newTravel = new Travels();
             $newTravel->destinazione = $faker->word();
             $newTravel->nazione_name = $faker->word();
             $newTravel->data_viaggio = $faker->dateTime();
             $newTravel->prezzo = $faker->randomFloat(2);
-            $newTravel->Save();
+            $newTravel -> save();
         }
     }
 }
