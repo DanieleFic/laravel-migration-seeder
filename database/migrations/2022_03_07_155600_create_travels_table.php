53c55447997a119dc1ccba0a8818d8c4c0459d9a
DanieleFic <reb_ficus22@hotmail.it>
2022-03-07 17:04:47 +0100

    first commit

---
+++ database/migrations/2022_03_07_155600_create_travels_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTravelsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('travels', function (Blueprint $table) {
+            $table->id();
+            $table->char('destinazione', 30);
+            $table->char('nazione_name', 30);
+            $table->date('data_viaggio');
+            $table->bigInteger('prezzo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('travels');
+    }
+}
---
+++ database/seeds/Travels.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+use Faker\Generator as Faker;
+use App\Travel;
+
+
+class Travels extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        for ($i=0; $i < 20; $i++) { 
+            $newTravel = new Travel();
+            $newTravel->destinazione = $faker->word();
+            $newTravel->nazione_name = $faker->word();
+            $newTravel->data_viaggio = $faker->dateTime();
+            $newTravel->prezzo = $faker->randomFloat(2);
+            $newTravel->Save();
+        }
+    }
+}
